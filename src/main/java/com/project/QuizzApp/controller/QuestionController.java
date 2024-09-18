425597d60bc0089454252e18a5dd4fe7721a96fa
Amaresh swamy <swamyamaresh123@gmail.com>
2024-09-18 22:19:23 +0530

    About the Controller info

--- src/main/java/com/project/QuizzApp/controller/QuestionController.java
+++ src/main/java/com/project/QuizzApp/controller/QuestionController.java
@@ -15,34 +15,36 @@ public class QuestionController
     @Autowired
     QuestionService questionService;
 
+
+    //Fetch the all questions
     @GetMapping("/allquestions")
     public List<Question> getAllQuestions()
     {
         return questionService.getAllQuestions();
     }
 
-
+    //Fetch the questions by category
     @GetMapping("/category/{category}")
     public List<Question> getQuestionsByCategory(@PathVariable String category)
     {
         System.out.println("start of by category");
         return questionService.getQuestionsByCategory(category);
     }
-
+    //Add the question
     @PostMapping("/add")
     public String addQuestion(@RequestBody Question question)
     {
         return questionService.addQuestions(question);
     }
 
-
+    //Update the question
     @PatchMapping("/update/{id}")
     public String updateQuestion(@PathVariable Integer id, @RequestBody Question question)
     {
         return questionService.updateQuestions(id,question);
     }
 
-
+    //Delete the question
     @DeleteMapping("/delete/{id}")
     public String deleteQuestion(@PathVariable Integer id)
     {
